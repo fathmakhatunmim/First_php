76733471ed304d3650fb588557da3919fbc4529b
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-10-27 17:40:41 +0600

    php

--- index.php
+++ index.php
@@ -12,9 +12,42 @@
 
        echo "MIM <br>";
 
-       $variable = "Fathma";
+       $variable = "Fathma <br>";
        echo $variable;
-    
-    ?>
+
+
+     $cars = array("Volvo", "BMW", "Toyota");
+    foreach($cars as $car){
+    echo $car . "<br>";
+    }
+      
+    $cars = array("Volvo", "BMW", "Toyota");
+    echo count($cars);
+    echo "<br>";
+
+    // update
+    $cars = array("Volvo", "BMW", "Toyota");
+    $cars[1] = "Ford";
+      foreach($cars as $car){
+    echo $car . "<br>";
+    }
+
+    //add array items
+    $cars = array("brand" => "Ford", "model" => "Mustang");
+    $cars["color"] = "Red";
+   foreach($cars as $car){
+    echo "<span style='display:block; margin-bottom:20px;'>$car</span>";
+}
+    //remove array items
+    $cars = array("Volvo", "BMW", "Toyota");
+    array_splice($cars, 1, 1);
+    foreach($cars as $car){
+    echo $car . "<br>";
+    }
+     
+
+      
+
+       ?>
 </body>
 </html>
